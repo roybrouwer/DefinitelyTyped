e7195d9fabae3dd4c5facdcac1d38b9e028f92d0
Roy Brouwer <roy.brouwer@source.ag>
2025-04-22 14:25:44 +0200

    feat: Add record_min_ms to type

--- types/mixpanel-browser/index.d.ts
+++ types/mixpanel-browser/index.d.ts
@@ -110,6 +110,7 @@ export interface Config {
     record_mask_text_class: string;
     record_mask_text_selector: string;
     record_max_ms: number;
+    record_min_ms: number;
     record_sessions_percent: number;
     record_canvas: boolean;
 }
